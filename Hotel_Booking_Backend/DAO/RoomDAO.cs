2c8171b47e85fae5b6feb2a5032ec6870c9198f0
Alex <protactini0819@gmail.com>
2023-06-29 10:07:34 -0700

    Fix Room Dao

--- Hotel_Booking_Backend/DAO/RoomDAO.cs
+++ Hotel_Booking_Backend/DAO/RoomDAO.cs
@@ -2,41 +2,53 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Hotel_Booking_Backend.Models;
 using Hotel_Booking_Backend.DAO;
+using Microsoft.EntityFrameworkCore;
 
 namespace Hotel_Booking_Backend.Services
 {
     public class RoomDAO
     {
-        private readonly RoomDAO _roomDAO;
+        private readonly HotelBookingContext _context;
 
-        public RoomDAO(RoomDAO roomDAO)
+        public RoomDAO(HotelBookingContext context)
         {
-            _roomDAO = roomDAO;
+            _context = context;
         }
 
         public async Task<IEnumerable<Room>> GetRoomByHotalIdAsync(int hotalId)
         {
-            return await _roomDAO.GetRoomByHotalIdAsync(hotalId);
+            return await _context.Rooms.Where(r => r.HotelId == hotalId).ToListAsync();
         }
 
         public async Task<Room> GetRoomByHotelIdAndRoomId(int hotelId, int roomId)
         {
-            return await _roomDAO.GetRoomByHotelIdAndRoomId(hotelId, roomId);
+            Room? room = await _context.Rooms.FirstOrDefaultAsync(r => r.HotelId == hotelId && r.Id == roomId);
+            return room;
         }
 
         public async Task<Room> AddRoomToHotel(int hotelId, Room room)
         {
-            return await _roomDAO.AddRoomToHotel(hotelId, room);
+            room.HotelId = hotelId;
+            _context.Rooms.Add(room);
+            await _context.SaveChangesAsync();
+            return room;
         }
 
         public async Task UpdateRoomInHotel(int hotelId, Room room)
         {
-            await _roomDAO.UpdateRoomInHotel(hotelId, room);
+            room.HotelId = hotelId;
+            _context.Entry(room).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
         }
 
         public async Task DeleteRoomFromHotel(int hotelId, int roomId)
         {
-            await _roomDAO.DeleteRoomFromHotel(hotelId, roomId);
+            var room = await _context.Rooms.FirstOrDefaultAsync(r => r.HotelId == hotelId && r.Id == roomId);
+            if (room != null)
+            {
+                _context.Rooms.Remove(room);
+                await _context.SaveChangesAsync();
+            }
         }
     }
 }
